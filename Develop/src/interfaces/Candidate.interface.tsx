4a3c7af700ae35af4a5733b2f50d51a7acd8abdd
Makayla J <makaylaj14@gmail.com>
2024-11-20 12:41:24 -0500

    added an Candidate Interface

--- Develop/src/interfaces/Candidate.interface.tsx
+++ Develop/src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,10 @@
 // TODO: Create an interface for the Candidate objects returned by the API
+export default interface Candidate {
+    readonly Name: string | null;
+    readonly Username: string | null;
+    readonly Location: string | null;
+    readonly Avatar: string | null;
+    readonly Email: string | null;
+    readonly Html_Url: string | null;
+    readonly Company: string | null;
+}
